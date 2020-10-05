ee9f73f43c6e2e74ca7ddb00cd1e80bbe8c8a88d
irekpi <pilat.irek@gmail.com>
2020-10-05 13:37:15 +0200

    Added doctor to pet and details as doctors list and pet

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, View, DetailView
 from django_filters.views import FilterView
 
 from users.forms import AddPetForm
@@ -18,6 +18,11 @@ class AddPetView(BaseContextMixinView, CreateView):
         return super().form_valid(form)
 
 
+class DetailPetView(DetailView):
+    template_name = 'users/detail_pet.html'
+    model = Pet
+
+
 class AllPetsView(FilterView):
     template_name = 'users/all_pets.html'
     paginate_by = 5
@@ -27,3 +32,34 @@ class AllPetsView(FilterView):
     def get_queryset(self):
         return PetsFilter(self.request.GET,
                           queryset=Pet.objects.filter(owner_name=self.request.user)).qs
+
+
+class AddDoctorList(FilterView):
+    template_name = 'users/add_doctor_list.html'
+    paginate_by = 5
+    login_url = reverse_lazy('account_login')
+    filterset_class = PetsFilter
+
+    def get_queryset(self):
+        return PetsFilter(self.request.GET,
+                          queryset=Pet.objects.filter(doctor=None)).qs
+
+
+class AddDoctor(View):
+    def dispatch(self, request, pk):
+        pet = Pet.objects.get(id=pk)
+        pet.doctor = self.request.user
+        pet.save()
+        view = AddDoctorList.as_view()
+        return view(request)
+
+
+class PatientsListView(FilterView):
+    template_name = 'users/patients_list.html'
+    paginate_by = 5
+    login_url = reverse_lazy('account_login')
+    filterset_class = PetsFilter
+
+    def get_queryset(self):
+        return PetsFilter(self.request.GET,
+                          queryset=Pet.objects.filter(doctor=self.request.user)).qs
